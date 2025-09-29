d59a14d9283cdc57b388a9eee9182e9c9eff58bd
KyriakosVerveridis <ververidiskyriakos@gmail.com>
2025-09-30 00:30:01 +0300

    final commit

--- calculator.py
+++ calculator.py
@@ -10,12 +10,21 @@ def get_multiply(num1,num2):
     result = num1 * num2
     print(int(result))
 
+def get_divide(num1,num2):
+    result = num1 / num2
+    print(int(result))
+
 while True:
     with open("epiloges.txt","r",encoding="utf-8") as file:
         content = file.read()
         print(content)
 
     operation = input("Κάνε μια επιλογή (πχ.1) για Πρόσθαιση: ")
+    while int(operation) < 1 or int(operation) > 5:
+        print("Μη έγκυρη επιλογη διαλεγε 1,2,3,4,5")
+        operation = input("Κάνε μια επιλογή (πχ.1) για Πρόσθαιση: ")
+    if operation.startswith("5"):
+        break
     num1 = float(input("Βάλε τον πρώτο αριθμό: "))
     num2 = float(input("Βάλε τον δεύτερο αριθμό: "))
 
@@ -25,5 +34,13 @@ while True:
         subtracked = get_subtracked(num1,num2)
     elif operation.startswith("3"):
         multiply = get_multiply(num1,num2)
+    else:
+        try:
+            divide = get_divide(num1,num2)
+        except ZeroDivisionError:
+            print("0")
+
+
+
 
 
