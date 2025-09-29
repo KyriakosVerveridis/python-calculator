3663de27eb3418a2bbbaf496a16347faccfdada4
KyriakosVerveridis <ververidiskyriakos@gmail.com>
2025-09-29 22:24:48 +0300

    Initial commit

---
+++ calculator.py
@@ -0,0 +1,16 @@
+# while True:
+with open("epiloges.txt","r",encoding="utf-8") as file:
+    content = file.read()
+    print(content)
+
+    # print("Επιλογές:\n"
+    #       "1.Πρόσθαιση\n"
+    #       "2.Αφαίρεση\n"
+    #       "3.Πολλαπλασιασμός\n"
+    #       "4.Διαίρεση\n"
+    #       "5.Έξοδος")
+
+    # operation = input("Κάνε μια επιλογή απο τα παρακάτω (πχ.1): ")
+    #
+    # num1 = float(input("Βάλε τον πρώτο αριθμό: "))
+    # num2 = float(input("Βάλε τον δεύτερο αριθμό: "))
